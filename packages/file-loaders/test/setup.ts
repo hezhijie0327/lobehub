541f335d2495ec8553ba9f5840cf8e44ca47e473
Zhijie He <hezhijie0327@hotmail.com>
2025-11-14 18:04:54 +0800

    fix: fix `@napi-rs/canvas` warning

--- packages/file-loaders/test/setup.ts
+++ packages/file-loaders/test/setup.ts
@@ -1,11 +1,3 @@
-// Polyfill DOMMatrix for pdfjs-dist in Node.js environment
-import { DOMMatrix } from '@napi-rs/canvas';
-
-if (typeof global.DOMMatrix === 'undefined') {
-  // @ts-ignore
-  global.DOMMatrix = DOMMatrix;
-}
-
 // Polyfill URL.createObjectURL and URL.revokeObjectURL for pdfjs-dist
 if (typeof global.URL.createObjectURL === 'undefined') {
   global.URL.createObjectURL = () => 'blob:http://localhost/fake-blob-url';
