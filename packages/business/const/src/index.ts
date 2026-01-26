d05abec9967e0c8db417f604a0343b036fe16944
Zhijie He <hezhijie0327@hotmail.com>
2026-01-26 20:12:57 +0800

    Update index.ts

--- packages/business/const/src/index.ts
+++ packages/business/const/src/index.ts
@@ -3,5 +3,5 @@ export * from './branding';
 export * from './llm';
 export * from './url';
 
-export const ENABLE_BUSINESS_FEATURES = false;
+export const ENABLE_BUSINESS_FEATURES = true;
 export const APPLY_OSS_FEATURES_PATCH = true;
